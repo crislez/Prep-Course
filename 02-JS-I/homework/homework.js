a8c4bf7536438c90273aaf1e6603730ad64f8651
cristian lezcano <cristianlezcano2000@hotmail.com>
2022-12-14 16:15:31 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = "HOLA";
 const nuevoNum = 5;
 
 // Crea una variable booleana:
-const nuevoBool = false;
+const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - null === 5;
